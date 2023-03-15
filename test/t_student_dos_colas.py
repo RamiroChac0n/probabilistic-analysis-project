5dda9e5b9e152a94dc8f8a74a0d3d1ebd379e010
Ramiro Chacon <ramiro20010202@gmail.com>
2023-03-15 16:13:32 -0600

    Mejora las gráficas de la distribución t-student

--- test/t_student_dos_colas.py
+++ test/t_student_dos_colas.py
@@ -3,8 +3,9 @@ import matplotlib.pyplot as plt
 from scipy.stats import t
 
 # Definir los grados de libertad
-gl = 5
-alpha = 0.1
+gl = 25
+alpha = 0.05
+
 # Generar una muestra aleatoria de la distribución t
 x = np.linspace(t.ppf(0.01, gl), t.ppf(0.99, gl), 100)
 
@@ -19,15 +20,15 @@ fig, ax = plt.subplots()
 ax.plot(x, t.pdf(x, gl), 'blue')
 
 # Graficar la línea vertical
-ax.axvline(x=0, color='black')
+ax.axvline(x=0, color='black', linewidth = 1)
 
 # Sombrear el área a la izquierda del valor crítico
-plt.fill_between(x, 0, t.pdf(x, gl), where=x<=t_critico_inferior, color='blue', alpha=0.3, label = "Área de rechazo")
+plt.fill_between(x, 0, t.pdf(x, gl), where=x<=t_critico_inferior, color='blue', alpha=0.3, label = "tc Inferior = {}".format(t_critico_inferior))
 
-plt.fill_between(x, 0, t.pdf(x, gl), where=x>=t_critico_superior, color='blue', alpha=0.3)
+plt.fill_between(x, 0, t.pdf(x, gl), where=x>=t_critico_superior, color='blue', alpha=0.3, label = "tc Superior = {}".format(t_critico_superior))
 
 # Graficar la línea vertical
-ax.axvline(x=-1.24, color='red', label="tp = {}".format(-1.24))
+ax.axvline(x=1.667, color='red', label="tp = {}".format(1.667))
 
 plt.legend()
 plt.show()
