0939efb260dffbc54dcbb5f0c1dd7a8d45248dfc
Ramiro <ramiro20010202@gmail.com>
2023-04-03 15:44:55 -0600

    Crea las funciones para las pruebas de hipotesis de proporciones

--- src/distribucion_normal_una_cola_izquierda.py
+++ src/distribucion_normal_una_cola_izquierda.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from scipy.stats import norm
 
-def una_cola_izquierda(media_muestral, media_poblacional, desviacion_std_poblacional, muestra, alpha):
+def una_cola_izquierda(media_muestral, media_poblacional, desviacion_std_poblacional, muestra, alpha, valor_esperado):
 
     z_prueba = (media_muestral - media_poblacional) / (desviacion_std_poblacional / np.sqrt(muestra))
 
@@ -24,14 +24,53 @@ def una_cola_izquierda(media_muestral, media_poblacional, desviacion_std_poblaci
     # Sombrar el área izquierda debajo de la curva
     plt.fill_between(x, 0, y, where=(x <= z_critico), color='blue', alpha=0.3, label = "Zc Inferior = {}".format(z_critico))
 
+    if valor_esperado == True:
+        # Graficar la línea vertical
+        ax.axvline(x=z_prueba, color='red', label = "Zp = {}".format(z_prueba))
+
+        # Calcula el P-valor
+        p_valor = norm.cdf(z_prueba)
+
+        # Sombrea el area del P-valor incluyendo el valor critico sin mezclar con el area de la cola
+        plt.fill_between(x, 0, y, where=(x <= z_prueba), color='skyblue', alpha=0.5, label = "P-valor = {}".format(p_valor))
+
+    plt.legend()
+    plt.xlabel('Valores x')
+    plt.ylabel('Densidad de probabilidad')
+    plt.title('Distribución normal estándar')
+    plt.savefig('grafica.jpg')
+
+def una_cola_izquierda_proporciones(proporcion_muestral, proporcion_poblacional, muestra, alpha, valor_esperado):
+
+    z_prueba = (proporcion_muestral - proporcion_poblacional) / np.sqrt((proporcion_poblacional * (1 - proporcion_poblacional)) / muestra)
+
+    # Crear un conjunto de valores x en el rango de -4 a 4 con incrementos de 0.01
+    x = np.arange(-4, 4, 0.01)
+
+    # Crear la distribución normal
+    y = norm.pdf(x, 0, 1)
+
+    # Graficar la distribución normal
+    fig, ax = plt.subplots()
+    ax.plot(x, y)
+
     # Graficar la línea vertical
-    ax.axvline(x=z_prueba, color='red', label = "Zp = {}".format(z_prueba))
+    ax.axvline(0, color='black', linewidth = 1)
 
-    # Calcula el P-valor
-    p_valor = norm.cdf(z_prueba)
+    z_critico = norm.ppf(alpha)
 
-    # Sombrea el area del P-valor incluyendo el valor critico sin mezclar con el area de la cola
-    plt.fill_between(x, 0, y, where=(x <= z_prueba), color='skyblue', alpha=0.5, label = "P-valor = {}".format(p_valor))
+    # Sombrar el área izquierda debajo de la curva
+    plt.fill_between(x, 0, y, where=(x <= z_critico), color='blue', alpha=0.3, label = "Zc Inferior = {}".format(z_critico))
+
+    if valor_esperado == True:
+        # Graficar la línea vertical
+        ax.axvline(x=z_prueba, color='red', label = "Zp = {}".format(z_prueba))
+
+        # Calcula el P-valor
+        p_valor = norm.cdf(z_prueba)
+
+        # Sombrea el area del P-valor incluyendo el valor critico sin mezclar con el area de la cola
+        plt.fill_between(x, 0, y, where=(x <= z_prueba), color='skyblue', alpha=0.5, label = "P-valor = {}".format(p_valor))
 
     plt.legend()
     plt.xlabel('Valores x')
