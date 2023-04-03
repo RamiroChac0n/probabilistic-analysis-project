f74c4e834bb840825f176896fb2b4d1800984505
Eduardo <cr4zsanchez@gmail.com>
2023-04-02 22:34:06 -0600

    A

--- test/pruebaPDF.py
+++ test/pruebaPDF.py
@@ -23,20 +23,11 @@ pdf.text(x = 65,y = 35, txt = 'Analisis Probabilistico')
 pdf.text(x = 65,y = 40, txt = 'Ing. Manuel Eduardo Alvarez Ruiz')
 pdf.text(x = 65,y = 45, txt = 'Proyecto Programa prueba de hipotesis')
 pdf.text(x = 65,y = 50, txt = 'Eduardo Rubén Cruz Sánchez      202146471')
-pdf.text(x = 65,y = 55, txt = 'Nery                           ---------')
-pdf.text(x = 65,y = 60, txt = 'Ramiro André Chaón Castañeda   201940859')
-pdf.text(x = 65,y = 65, txt = 'Kenat                          ---------')
+pdf.text(x = 65,y = 55, txt = 'Nery José Galdámez Aristondo     202140502')
+pdf.text(x = 65,y = 60, txt = 'Ramiro André Chacón Castañeda   201940859')
+pdf.text(x = 65,y = 65, txt = 'Kenat Jesiel Pérez Lucas        202040366')
 #IMAGEN
-pdf.image('logotipo-cunori-transparente.png',x = 27,y = 29,w = 33, h = 33)
-pdf.image('prueba.jpg',x = 50,y = 175,w = 120, h = 90)
-
-
-
-''''
-
-
-#ELIPSE
-pdf.ellipse(x= 10,y = 15, w = 100,h =80)
-'''
+pdf.image('img/logotipo-cunori-transparente.png',x = 27,y = 29,w = 33, h = 33)
+#pdf.image('prueba.jpg',x = 50,y = 175,w = 120, h = 90)
 
 pdf.output('hoja.pdf')
